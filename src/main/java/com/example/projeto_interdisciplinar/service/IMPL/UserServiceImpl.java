5436163b8bb9546c592d2f35a4a3ded56c1d8778
vinicius1diniz <vinicius.diniz@germinare.org.br>
2023-09-25 16:50:54 -0300

    subindo ultimas alterações

--- src/main/java/com/example/projeto_interdisciplinar/service/IMPL/UserServiceImpl.java
+++ src/main/java/com/example/projeto_interdisciplinar/service/IMPL/UserServiceImpl.java
@@ -1,46 +1,47 @@
-package com.example.projeto_interdisciplinar.service.IMPL;
+    package com.example.projeto_interdisciplinar.service.IMPL;
 
-import com.example.projeto_interdisciplinar.entity.Usuario;
-import com.example.projeto_interdisciplinar.repo.UserRepo;
-import com.example.projeto_interdisciplinar.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service; // Import the Service annotation
+    import com.example.projeto_interdisciplinar.entity.Usuario;
+    import com.example.projeto_interdisciplinar.repo.UserRepo;
+    import com.example.projeto_interdisciplinar.service.UserService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service; // Import the Service annotation
 
-import java.util.List;
-import java.util.Optional;
+    import java.util.List;
+    import java.util.Objects;
+    import java.util.Optional;
 
-@Service
-public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepo userRepo;
+    @Service
+    public class UserServiceImpl implements UserService {
+        @Autowired
+        private UserRepo userRepo;
 
-    @Override
-    public List<Usuario> getUser() {
-        return userRepo.findAll();
-    }
-    public boolean verifyAccount( String email, String password){
-        Usuario usuario = userRepo.findByEmail(email);
-        if (usuario != null && usuario.getSenha() == password){
-            return true;
+        @Override
+        public List<Usuario> getUser() {
+            return userRepo.findAll();
         }
-        return false;
-    }
-    @Override
-    public Usuario addUser(Usuario user) {
-        return userRepo.save(user);
-    }
+        public boolean verifyAccount( String email, String password){
+            Usuario usuario = userRepo.findByEmail(email);
+            if (usuario != null && Objects.equals(usuario.getSenha(), password)){
+                return true;
+            }
+            return false;
+        }
+        @Override
+        public Usuario addUser(Usuario user) {
+            return userRepo.save(user);
+        }
 
-    @Override
-    public void updateEmail(int id, String email) {
-        Optional<Usuario> user1 = userRepo.findById(id);
-        if (user1.isPresent()){
-            Usuario usuario = user1.get();
-            usuario.setEmail(email);
-            userRepo.save(usuario);
+        @Override
+        public void updateEmail(int id, String email) {
+            Optional<Usuario> user1 = userRepo.findById(id);
+            if (user1.isPresent()){
+                Usuario usuario = user1.get();
+                usuario.setEmail(email);
+                userRepo.save(usuario);
+            }
+        }
+        @Override
+        public void delectUser(int id) {
+            userRepo.deleteById(id);
         }
     }
-    @Override
-    public void delectUser(int id) {
-        userRepo.deleteById(id);
-    }
-}
