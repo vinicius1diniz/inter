92b6b81a0037e45b0d7051f9be81fd3a545c8ac2
Vinicius Diniz <viniciusdiniz-ieg@instituto.ad>
2023-10-03 14:43:09 -0300

    Comentando uso do mongo por momento

--- src/main/java/com/example/projeto_interdisciplinar/controller/UserController.java
+++ src/main/java/com/example/projeto_interdisciplinar/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.projeto_interdisciplinar.controller;
 
 import com.example.projeto_interdisciplinar.entity.Usuario;
-import com.example.projeto_interdisciplinar.service.LogService;
+//import com.example.projeto_interdisciplinar.service.LogService;
 import com.example.projeto_interdisciplinar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,26 +14,26 @@ public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
-    private LogService logService;
+//    private LogService logService;
     @GetMapping("/all")
     public List<Usuario> getUsers(){
         return userService.getUser();
     }
     @PostMapping("/insert")
     public Usuario insert(@RequestBody Usuario user){
-        logService.generateLog("Created account", user.getId());
+//        logService.generateLog("Created account", user.getId());
         return userService.addUser(user);
     }
     @PutMapping("/update/{id}/{email}")
     public ResponseEntity updateEmail(@PathVariable int id, @PathVariable String email){
         userService.updateEmail(id, email);
-        logService.generateLog("update email", id);
+//        logService.generateLog("update email", id);
         return ResponseEntity.ok("Email atualizado");
     }
     @DeleteMapping("/delete/{id}")
     public ResponseEntity delete(@PathVariable int id){
         userService.delectUser(id);
-        logService.generateLog("delete account", id);
+//        logService.generateLog("delete account", id);
         return ResponseEntity.ok("Usuario excluido");
     }
     @GetMapping("/verifyaccount")
